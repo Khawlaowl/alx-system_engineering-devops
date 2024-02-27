6f628c5238225ea9a0eda5d2f6b05220554ef105
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-02-27 19:29:38 +0100

    f

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# 1-install_a_package.pp
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask using pip3
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Notify the user
+notify { 'Flask_installed':
+  message => 'Flask 2.1.0 installed successfully',
 }
