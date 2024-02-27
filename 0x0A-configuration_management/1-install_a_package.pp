b80a6de5c55be94ab6be83d697a4c55a1a1f37ad
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-02-27 19:43:13 +0100

    f

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
 # 1-install_a_package.pp
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
-}
+
 # Ensure pip3 is installed
 package { 'python3-pip':
   ensure => installed,
@@ -14,7 +11,8 @@ package { 'Flask':
   provider => 'pip3',
 }
 
-# Notify the user
-notify { 'Flask_installed':
-  message => 'Flask 2.1.0 installed successfully',
+# Install puppet-lint gem
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
